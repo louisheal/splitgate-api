42bdc0411cc004b365a9ca55f557ba52d3164d04
Louis Heal <lh420@ic.ac.uk>
2023-03-09 21:49:11 +0000

    feat: implement skeleton for contacting splitgate api

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, jsonify, request
 import requests
+import os
 
 app = Flask(__name__)
 
+url = 'https://public-api.tracker.gg/v2/splitgate/standard/profile'
+platform = 'steam'
+username = '76561198059390699'
+header = {"TRN-Api-Key":os.getenv("api_key")}
+
 @app.route('/')
 def home():
-    return "Hello world!"
+    response = requests.get(url + '/' + platform + '/' + username, headers = header)
+    response = jsonify(response)
+    return response
