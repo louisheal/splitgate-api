81949c86cac373427a2d16e83cdbe8dd075b4a0f
Louis Heal <lh420@ic.ac.uk>
2023-03-10 12:49:16 +0000

    docs: explain why the validation methods work

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-url = """https://public-api.tracker.gg/v2/splitgate/standard/profile"""
+url = "https://public-api.tracker.gg/v2/splitgate/standard/profile"
 header = {'TRN-Api-Key':os.getenv('api_key')}
 
 @app.route('/')
@@ -19,8 +19,10 @@ def home():
     else:
         return "Argument \"platform\" not set", 400
 
+"""Splitagte is only available on PC/Steam Xbox and Playstation"""
 def validate_platform(platform):
     return platform in ['steam', 'xbl', 'ps4']
 
+"""SteamID64 usernames are only made up of digits"""
 def validate_username(username):
     return username.isdigit()
